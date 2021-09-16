013b95d08de94abf8c0a27a7ed95f0f235477c3a
Luís Fernando Ferreira Pereira Lopes <devthumos@gmail.com>
2021-09-16 17:23:17 -0300

    Delete src directory

--- src/Tkinter/main_carteira.py
+++
@@ -1,4 +0,0 @@
-from window_carteira_class import Carteira
-
-if __name__ == "__main__":
-    jan_crtr = Carteira()
--- src/Tkinter/main_login.py
+++
@@ -1,4 +0,0 @@
-from window_login_class import Login
-
-if __name__ == "__main__":
-    jan_lgn = Login()

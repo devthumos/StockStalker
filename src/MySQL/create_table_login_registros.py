c3cc05357c06ec56763f87c1d1dfd997d3452ab9
Luís Fernando Ferreira Pereira Lopes <devthumos@gmail.com>
2021-10-03 23:12:07 -0300

    Update create_table_login_registros.py

--- src/MySQL/create_table_login_registros.py
+++ src/MySQL/create_table_login_registros.py
@@ -16,4 +16,8 @@ connection = pymysql.connect(
 
 cursor = connection.cursor()
 cursor.execute("CREATE TABLE login(user VARCHAR(255), password VARCHAR(255))")
-cursor.execute("CREATE TABLE registros(codigo VARCHAR(255), tipo VARCHAR(255), empresa VARCHAR(255))")
+cursor.execute(f"CREATE TABLE registros(codigo VARCHAR(255), tipo VARCHAR(255), PRIMARY KEY(codigo))")
+for i in range(1, 5):
+    cursor.execute(f"ALTER TABLE registros ADD indicador_0{i} VARCHAR(255)")
+for i in range(1, 5):
+    cursor.execute(f"ALTER TABLE registros ADD label_0{i} VARCHAR(255)")

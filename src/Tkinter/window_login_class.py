cd2a58114e60cb6e7be03625aac588511cdad70f
Luís Fernando Ferreira Pereira Lopes <devthumos@gmail.com>
2021-10-04 03:54:13 -0300

    Update window_login_class.py

--- src/Tkinter/window_login_class.py
+++ src/Tkinter/window_login_class.py
@@ -20,9 +20,9 @@ class LoginWindow():
         wndlogin.rowconfigure(0, weight=1)
 
         # Frame que conterá a label "LOGIN", label "LOGIN"
-        lbl_lgin_frm = Frame(mainframe, bg="#58636E")
-        lbl_log = Label(lbl_lgin_frm, text="LOGIN", background="#58636E", fg="white",
-                            font=Font(family="Comic Sans Ms", size=15, weight="bold"))
+        lbl_lgin_frm = Frame(mainframe, bg="Black")
+        lbl_log = Label(lbl_lgin_frm, text="LOGIN", background="Black", fg="white",
+                            font=Font(family="Arial Black", size=15, weight="bold"))
         lbl_log.pack()
         lbl_lgin_frm.pack(fill=X, side=TOP)
 
@@ -30,31 +30,35 @@ class LoginWindow():
         lgn_frm = Frame(mainframe, pady=10)
         # Parte Usuário
         cls.uservar = StringVar()
-        ttk.Label(lgn_frm, text="Usuário",
-                  font=Font(family="Comic Sans Ms", size=10, weight="normal")).grid(column=0, row=0, sticky=(E))
+        ttk.Label(lgn_frm, text="Usuário",font=Font(family="Arial Black", size=10, weight="normal",
+                                                    underline=True)).grid(column=0, row=0, sticky=(E))
         user_entry = Entry(lgn_frm, width=20, textvariable=cls.uservar)
         user_entry.grid(column=1, row=0, sticky=(W))
         user_entry.grid_configure(padx=5, pady=5)
         # Parte Senha
         cls.passvar = StringVar()
         ttk.Label(lgn_frm, text="Senha",
-                  font=Font(family="Comic Sans Ms", size=10, weight="normal")).grid(column=0, row=1, sticky=(E))
+                  font=Font(family="Arial Black", size=10, weight="normal",
+                            underline=True)).grid(column=0, row=1, sticky=(E))
         pass_entry = Entry(lgn_frm, width=20, textvariable=cls.passvar)
         pass_entry.grid(column=1, row=1, sticky=(W))
         pass_entry.grid_configure(padx=5, pady=5)
+
         # Botão "Logar"
-        btn_log = Button(lgn_frm, text="Logar", bg="#58636E", fg="white", relief="flat", command=cls.logar)
+        btn_log = Button(lgn_frm, text="Logar", bg="Black", fg="white", relief="flat", command=cls.logar,
+                         font=Font(family="Arial Black", size=9, weight="normal"))
         btn_log.grid(column=1, row=3, sticky=(N, W, S, E), columnspan=1)
         btn_log.grid_configure(padx=5, pady=5)
         # Botão "Não Tenho Cadastro"
-        btn_regst = Button(lgn_frm, text="Registrar", bg="#58636E", fg="white", relief="flat", command=cls.registrar)
+        btn_regst = Button(lgn_frm, text="Registrar", bg="Black", fg="white", relief="flat", command=cls.registrar,
+                           font=Font(family="Arial Black", size=9, weight="normal"))
         btn_regst.grid(column=1, row=4, sticky=(N, W, S, E), columnspan=1)
+        lgn_frm.pack()
+        lgn_frm.pack_configure(padx="0 52")
         btn_regst.grid_configure(padx=5, pady=5)
         # Label Não Estática "ACESSO PERMITIDO", "ACESSO NEGADO", "ACESSO REGISTRADO"
-        cls.lbl = Label(lgn_frm, text="")
-        cls.lbl.grid(column=1, row=5, sticky=(N, W, S, E), columnspan=1)
-        cls.lbl.grid_configure(padx=5, pady=5)
-        lgn_frm.pack()
+        cls.lbl = Label(mainframe, text="")
+        cls.lbl.pack()
 
         # Botão Quit
 

bff8e913d5a760a5a7a7b8134c21a06ac6223618
Luís Fernando Ferreira Pereira Lopes <devthumos@gmail.com>
2021-10-04 03:53:15 -0300

    Update window_carteira_class.py

--- src/Tkinter/window_carteira_class.py
+++ src/Tkinter/window_carteira_class.py
@@ -1,109 +1,91 @@
 from tkinter import *
 from tkinter.font import *
 from tkinter import ttk
-from src.MySQL.registro import RegistroMySQL
-from src.Web_Scraping.statusinvest import Webscrapper
+from src.MySQL.login import LoginMySQL
 
 
-class WindowCarteira():
+class LoginWindow():
 
 
     def __init__(cls):
-        cls.object_registro = RegistroMySQL()
+        cls.object_login = LoginMySQL()
 
-        wndcarteira = Tk()
-        wndcarteira.title("ActionStalker")
-        wndcarteira.geometry("925x300+100+100")
+        wndlogin = Tk()
+        wndlogin.title("ActionStalker")
+        wndlogin.geometry("500x250+100+100")
 
-        # Frame principal "mainframe"
-        mainframe = ttk.Frame(wndcarteira)
+        mainframe = ttk.Frame(wndlogin)
         mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
-        wndcarteira.columnconfigure(0, weight=1)
-        wndcarteira.rowconfigure(0, weight=1)
+        wndlogin.columnconfigure(0, weight=1)
+        wndlogin.rowconfigure(0, weight=1)
 
-        # Frame "lbl_lgin_frm" que conterá label e button, label "LOGIN", button "Config"
-        # Frame lbl_lgin_frm
-        lbl_lgin_frm = Frame(mainframe, bg="black")
-        lbl_lgin_frm.pack(fill=X)
-        lbl_lgin_frm.columnconfigure(0, weight=1)
-        lbl_lgin_frm.rowconfigure(0, weight=1)
-        # Label "CARTEIRA STALKER"
-        lbl_log = Label(lbl_lgin_frm, text="CARTEIRA STALKER", background="black", fg="white",
-                        font=Font(family="Arial Black", size=20, weight="bold"))
+        # Frame que conterá a label "LOGIN", label "LOGIN"
+        lbl_lgin_frm = Frame(mainframe, bg="Black")
+        lbl_log = Label(lbl_lgin_frm, text="LOGIN", background="Black", fg="white",
+                            font=Font(family="Arial Black", size=15, weight="bold"))
         lbl_log.pack()
-        lbl_log.pack_configure()
-        # Label "img_sett"
-        # lbl_sett = Label(lbl_lgin_frm, text="img_sett", background="black", fg="white",
-        #                  font=Font(family="Comic Sans Ms", size=15, weight="bold"))
-        # lbl_sett.grid(row=0, column=1, sticky=(E))
-        # lbl_sett.grid_configure(padx=5)
+        lbl_lgin_frm.pack(fill=X, side=TOP)
 
-        # Frame "rgst_cod" que conterá labels, button, Entry Widgets
-        # Frame rgst_cod
-        rgst_cod = Frame(mainframe)
-        rgst_cod.pack(fill=X)
-        rgst_cod.columnconfigure(0, weight=1)
-        rgst_cod.rowconfigure(0, weight=1)
-        # Frame "space_frm" responsável pelo espaçamento dentro do Frame "rgst_cod"
-        space_frm = Frame(rgst_cod)
-        space_frm.pack()
-        space_frm.pack_configure(padx="0 70")
-        # Label text="Código" e Entry "cod_entry"
-        cls.codvar = StringVar()
-        ttk.Label(space_frm, text="Código", foreground="black",
-                  font=Font(family="Arial Black", size=12, weight="bold", underline=True)).grid(row=0, column=0, sticky=(E))
-        cod_entry = Entry(space_frm, width=51, textvariable=cls.codvar)
-        cod_entry.grid(row=0, column=1, sticky=(W))
-        # Button "rgst_btn" e Legendas, não vou fazer as legendas agora
-        rgst_btn = Button(space_frm, text="Registrar", bg="black", fg="white", command=cls.registrar,
-                          font=Font(family="Arial Black", size=10, weight="normal")) # TESTE
-        rgst_btn.grid(row=1, column=1, sticky=(N, S, W, E))
+        # Frame que conterá as Labels Estáticas e os Widgets de Entrada
+        lgn_frm = Frame(mainframe, pady=10)
+        # Parte Usuário
+        cls.uservar = StringVar()
+        ttk.Label(lgn_frm, text="Usuário",font=Font(family="Arial Black", size=10, weight="normal",
+                                                    underline=True)).grid(column=0, row=0, sticky=(E))
+        user_entry = Entry(lgn_frm, width=20, textvariable=cls.uservar)
+        user_entry.grid(column=1, row=0, sticky=(W))
+        user_entry.grid_configure(padx=5, pady=5)
+        # Parte Senha
+        cls.passvar = StringVar()
+        ttk.Label(lgn_frm, text="Senha",
+                  font=Font(family="Arial Black", size=10, weight="normal",
+                            underline=True)).grid(column=0, row=1, sticky=(E))
+        pass_entry = Entry(lgn_frm, width=20, textvariable=cls.passvar)
+        pass_entry.grid(column=1, row=1, sticky=(W))
+        pass_entry.grid_configure(padx=5, pady=5)
 
-        # Frame Combobox "cbx_frn" que conterá o combobox e os scrollbars
-        # Frame Combobox "cbx_frn"
-        cbx_frn = Frame(mainframe, padx="10", pady="10")
-        cbx_frn.pack(fill=BOTH)
-        cbx_frn.columnconfigure(0, weight=1)
-        cbx_frn.rowconfigure(0, weight=1)
-        # Combobox, POR ENQUANTO VAI SER COM LIST
-        cls.cbx = Listbox(cbx_frn)
-        # Scrollbar Vertical e Scrollbar Horizontal
-        scrl_vrtcl = Scrollbar(cbx_frn, orient=VERTICAL, command=cls.cbx.yview)
-        scrl_hrntl = Scrollbar(cbx_frn, orient=HORIZONTAL, command=cls.cbx.xview)
-        scrl_vrtcl.pack(side=RIGHT, fill=Y)
-        scrl_hrntl.pack(side=BOTTOM, fill=X)
-        # Configurando os Scollbars na Combobox
-        cls.cbx.configure(yscrollcommand=scrl_vrtcl.set)
-        cls.cbx.configure(xscrollcommand=scrl_hrntl.set)
-        cls.cbx.pack(fill=BOTH, expand=True)
+        # Botão "Logar"
+        btn_log = Button(lgn_frm, text="Logar", bg="Black", fg="white", relief="flat", command=cls.logar,
+                         font=Font(family="Arial Black", size=9, weight="normal"))
+        btn_log.grid(column=1, row=3, sticky=(N, W, S, E), columnspan=1)
+        btn_log.grid_configure(padx=5, pady=5)
+        # Botão "Não Tenho Cadastro"
+        btn_regst = Button(lgn_frm, text="Registrar", bg="Black", fg="white", relief="flat", command=cls.registrar,
+                           font=Font(family="Arial Black", size=9, weight="normal"))
+        btn_regst.grid(column=1, row=4, sticky=(N, W, S, E), columnspan=1)
+        lgn_frm.pack()
+        lgn_frm.pack_configure(padx="0 52")
+        btn_regst.grid_configure(padx=5, pady=5)
+        # Label Não Estática "ACESSO PERMITIDO", "ACESSO NEGADO", "ACESSO REGISTRADO"
+        cls.lbl = Label(mainframe, text="")
+        cls.lbl.pack()
 
-        # Pré adicionando todos os elementos do banco de dados na listbox
-        # VAMO VER
-        registros = cls.object_registro.list_elements()
-        for i in registros:
-            str_statement = f"CÓDIGO:{i[0]} | TIPO:{i[1]} | {i[6]}:{i[2]} | {i[7]}:{i[3]} | {i[8]}:{i[4]} | {i[9]}:{i[5]}"
-            cls.cbx.insert(END, str_statement)
+        # Botão Quit
 
-        # POLIMENTOS
-        # Espaçamento dentro do frame "space_frm" que é child do frame "rgst_cod"
-        for child in space_frm.winfo_children():
-            child.grid_configure(padx=5)
-            """# Espaçamento dentro do frame comboxox "cbx_frn"
-        for child in cbx_frn.winfo_children():
-            child.pack_configure(pady=10, padx=5)"""
-
-        # Startando o Loopping
-        wndcarteira.mainloop()
+        wndlogin.bind("<Return>", cls.logar)
+        wndlogin.mainloop()
 
 
-    def into_wbscrapper(cls):
-        wb = Webscrapper()
-        indicadores = wb.main(cls.codvar.get())
-        msg = f"CÓDIGO:{cls.codvar.get().replace('-', ' ')} | TIPO:{indicadores[1]} | {indicadores[0][0][0]}:{indicadores[0][0][1]} | {indicadores[0][1][0]}:{indicadores[0][1][1]} | {indicadores[0][2][0]}:{indicadores[0][2][1]} | {indicadores[0][3][0]}:{indicadores[0][3][1]}"
-        return indicadores, msg
+    def logar(cls, *args):
+        credenciais = cls.object_login.verificacao_login(cls.uservar.get(), cls.passvar.get())
+        cls.passvar.set("")
+        cls.uservar.set("")
+        if credenciais:
+            cls.lbl["text"] = "ACESSO PERMITIDO"
+        else:
+            cls.lbl["text"] = "ACESSO NEGADO"
 
-    # TESTE, EMPRESA DEVE SER COLOCADA A PARTIR DO WEBSCRAPPER
-    def registrar(cls, *args):
-        stt = cls.into_wbscrapper()
-        if cls.object_registro.registrar_registros(cls.codvar.get(), stt[0]) != False:
-            cls.cbx.insert(0, stt[1])
+
+
+    def registrar(cls):
+        registro = cls.object_login.registrar_login(cls.uservar.get(), cls.passvar.get())
+
+        if registro == None:
+            cls.lbl["text"] = f"Usuário \"{cls.uservar.get()}\" Já Existente, Insira Outro Usuário"
+        elif registro == True:
+            cls.lbl["text"] = f"Usuário \"{cls.uservar.get()}\" Cadastrado Com Sucesso"
+        else:
+            cls.lbl["text"] = f"ERRO, Não Foi Possível Registrar o Usuário \"{cls.uservar.get()}\""
+
+        cls.passvar.set("")
+        cls.uservar.set("")
